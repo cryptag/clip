9a8f5a110b68b0be9374b1048c88880eaa5252f5
Steve Phillips <steve@tryingtobeawesome.com>
2017-02-09 01:41:09 -0800

    v0.5: URL of original page now added to top of ~note

--- content.js
+++ content.js
@@ -1,6 +1,11 @@
+var selection = window.getSelection().toString();
+if (selection) {
+    selection = '> ' + selection + '\n\n'
+}
+
 // Send a message containing the page details back to the event page
 chrome.runtime.sendMessage({
     'title': document.title,
     'url': window.location.href,
-    'summary': window.getSelection().toString() ? '> ' + window.getSelection().toString() + '\n\n' : ''
+    'summary': 'URL: <' + window.location.href + '>\n\n' + selection
 });
